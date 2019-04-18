ebf51b19dc81a01dfaabab409d1ed683f22f576c
Jamie5156 <John.4656@163.com>
2019-04-18 11:12:13 +0800

    第一次提交

---
+++ FirstProgram/Vanch.FirstProgram/Vanch.FirstProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vanch.FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => Console.WriteLine("Hello VANCH"));
+        }
+    }
+}
